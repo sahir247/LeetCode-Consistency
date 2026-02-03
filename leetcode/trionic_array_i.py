614380b81243bceb2f16e91f779c4fee4e2fd29b
sahir247 <sahir247@users.noreply.github.com>
2026-02-04 05:20:40 +0530

    leetcode_problem: Daily LeetCode (extracted): Trionic Array I

--- leetcode/trionic_array_i.py
+++ leetcode/trionic_array_i.py
@@ -5,26 +5,23 @@ class Solution:
         n = len(nums)
         if n < 3:
             return False
-        p, q = -1, -1
-        for i in range(1, n - 1):
-            if nums[i - 1] < nums[i] and nums[i] > nums[i + 1]:
-                p = i
-                break
-        if p == -1:
-            return False
-        for i in range(p + 1, n - 1):
-            if nums[i] > nums[i + 1] and nums[i + 1] < nums[i + 2]:
-                q = i + 1
-                break
-        if q == -1:
-            return False
-        for i in range(1, p):
-            if nums[i - 1] >= nums[i]:
-                return False
-        for i in range(p, q):
-            if nums[i] <= nums[i + 1]:
-                return False
-        for i in range(q, n - 1):
-            if nums[i] >= nums[i + 1]:
-                return False
-        return True
+        
+        trend = 0  # 0: unknown, 1: increasing, -1: decreasing
+        peak_trough_count = 0
+        for i in range(1, n):
+            if nums[i] > nums[i-1]:
+                if trend == -1:  # Switch from decreasing to increasing
+                    peak_trough_count += 1
+                    if peak_trough_count > 2:
+                        return False
+                trend = 1
+            elif nums[i] < nums[i-1]:
+                if trend == 1:  # Switch from increasing to decreasing
+                    peak_trough_count += 1
+                    if peak_trough_count > 2:
+                        return False
+                trend = -1
+            else:
+                return False  # Elements must be distinct
+        
+        return peak_trough_count == 2
