b1d67e6e893c88d243bc8cad2d9e9164dc81fe32
sahir247 <sahir247@users.noreply.github.com>
2026-02-05 05:08:22 +0530

    leetcode_problem: Daily LeetCode (extracted): Trionic Array II

--- leetcode/trionic_array_ii.py
+++ leetcode/trionic_array_ii.py
@@ -1,24 +1,19 @@
 from typing import List
 
 class Solution:
-    def maxSumTrionic(self, nums: List[int]) -> int:
+    def solution(self, nums: List[int]) -> int:
         n = len(nums)
+        left_min_sum = [0]*n
+        right_min_sum = [0]*n
+        min_sum = 0
+        for i in range(n):
+            min_sum = min(min_sum, 0) + nums[i]
+            left_min_sum[i] = min_sum
+        min_sum = 0
+        for i in range(n-1, -1, -1):
+            min_sum = min(min_sum, 0) + nums[i]
+            right_min_sum[i] = min_sum
         max_sum = float('-inf')
-        for l in range(n-3):
-            sum_first_segment = sum(nums[l:l+1])
-            min_sum_second_segment = float('inf')
-            max_sum_second_segment = float('-inf')
-            sum_second_segment = 0
-            for p in range(l+1, n-2):
-                sum_second_segment += nums[p]
-                min_sum_second_segment = min(min_sum_second_segment, sum_second_segment)
-                max_sum_second_segment = max(max_sum_second_segment, sum_second_segment)
-                for q in range(p+1, n-1):
-                    sum_third_segment = sum(nums[q:q+1])
-                    for r in range(q+1, n):
-                        sum_third_segment += nums[r]
-                        current_sum = max(0, sum_first_segment) + max_sum_second_segment + max(0, sum_third_segment)
-                        max_sum = max(max_sum, current_sum)
-                        current_sum = max(0, sum_first_segment) + sum_second_segment + max(0, sum_third_segment)
-                        max_sum = max(max_sum, current_sum)
+        for i in range(1, n-1):
+            max_sum = max(max_sum, left_min_sum[i-1] + nums[i] + right_min_sum[i+1])
         return max_sum
